5836dff12b7fa66c51ef2593dffcad4391293f7d
Yuumi <yuuming.n@gmail.com>
2018-12-17 19:55:02 +0900

    Add default states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todolist: []
+    };
+  }
   render() {
     return (
       <div className="App">
